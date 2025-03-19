ff6c2221c64001f222b5df3d5214da27819cdbe4
Andy Wertner <andy@tavus.io>
2025-03-19 15:11:49 +0000

    update

--- public-website/src/scripts.js
+++ public-website/src/scripts.js
@@ -5,12 +5,17 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(response => response.json())
         .then(repos => {
             repos.forEach(repo => {
-                if (repo.has_pages) {
+                if (repo.has_pages && !repo.name.startsWith('x')) {
                     const listItem = document.createElement('li');
                     const link = document.createElement('a');
                     link.href = `https://${repo.owner.login}.github.io/${repo.name}`;
                     link.textContent = repo.name;
                     listItem.appendChild(link);
+
+                    const description = document.createElement('p');
+                    description.textContent = repo.description;
+                    listItem.appendChild(description);
+
                     repoList.appendChild(listItem);
                 }
             });
